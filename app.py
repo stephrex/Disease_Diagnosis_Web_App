f3fb5f99debd4a78c194be58aaecc374324938bb
stephrex <stephrex602@gmail.com>
2024-10-31 14:14:11 +0100

    updating

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ CORS(app)
 # Downloading necessary NLTK data
 nltk.download('punkt')
 nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger_eng')
 nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
